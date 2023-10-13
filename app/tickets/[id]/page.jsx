e897fefa236b2dc1bfe85c0aee6f45de3004f05d
rl627 <rl627@drexel.edu>
2023-10-13 10:12:17 -0400

    fixed ticket dynamic segment

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,31 +1,37 @@
 async function getTicket(id) {
-  const res = await fetch(`http://localhost:4000/tickets/${id}`, {
+  const res = await fetch('http://localhost:40000/tickets', {
     next: {
-      revalidate: 60
+      revalidate: 0 // use 0 to opt out of using cache
     }
   })
-
-  return res.json()
+  const tickets = await res.json()
+  const ticket = tickets.find((ticket) => ticket.id === id)
+ 
+  return (ticket)
+    
 }
 
 
 export default async function TicketDetails({ params }) {
   // const id = params.id
-  const ticket = await getTicket(params.id)
+  const ticket = await getTicket(params.id) 
 
-  return (
-    <main>
+  const details = ticket && (
+    <div>
       <nav>
         <h2>Ticket Details</h2>
       </nav>
       <div className="card">
         <h3>{ticket.title}</h3>
-        <small>Created by {ticket.user_email}</small>
-        <p>{ticket.body}</p>
-        <div className={`pill ${ticket.priority}`}>
-          {ticket.priority} priority
-        </div>
+        
       </div>
+    </div>
+  )
+
+  return (
+    <main> 
+      {/* {params.id} */}
+      {details}
     </main>
   )
 }
