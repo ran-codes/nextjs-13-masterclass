c08941267aa4f1051f6c2ce7df76c8cd3e76682a
rl627 <rl627@drexel.edu>
2023-10-16 09:36:24 -0400

    applied `group routes`

--- app/(dashboard)/tickets/TicketList.jsx
+++ app/(dashboard)/tickets/TicketList.jsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 async function getTickets() {
   // imitate delay
-  await new Promise(resolve => setTimeout(resolve, 3000))
+  // await new Promise(resolve => setTimeout(resolve, 3000))
 
   
   const res = await fetch('http://localhost:40000/tickets', {
