7dfd9a55f0ae59ffe59ccfb31b07d27ba723310a
rl627 <rl627@drexel.edu>
2023-10-16 13:54:58 -0400

    get session information and pass to Navbar

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,11 +1,18 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import Navbar from "../components/Navbar";
+import { cookies }  from "next/headers"
 
- 
 
-export default function DashboardLayout({ children }) {
+
+export default async function DashboardLayout({ children }) {
+
+  const supabase = createServerComponentClient({ cookies});
+  const { data } = await supabase.auth.getSession()
+  console.log(data)
+
   return (
     <>
-      <Navbar />
+      <Navbar user={data.session.user} />
       {children}
     </>
   )
